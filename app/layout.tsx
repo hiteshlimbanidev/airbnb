3437329bdb2a1b686ecf8e1c0b8a78b8e88bfc60
hitesh <hiteshbusinessacc@gmail.com>
2023-04-27 17:55:14 +0530

    new component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,30 @@
-import Navbar from './components/navbar/Navbar'
-import './globals.css'
-import { Nunito } from 'next/font/google'
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
 
-const inter = Nunito({ subsets: ['latin'] })
+const inter = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
-}
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />{children}</body>
+        <ClientOnly>
+          <Modal title="hello" isOpen/>
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
