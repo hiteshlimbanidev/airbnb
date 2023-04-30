d04c8561124ce9596ca11a13ad67175d1ced9d19
hiteshlimbanidev <hiteshbusinessacc@gmail.com>
2023-04-30 11:49:16 +0530

    heading

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -10,7 +10,7 @@ const Heading: React.FC<HeadingProps> = ({ title, center, subTitle }) => {
   return (
     <div className={center ? "text-center" : "text-start"}>
       <div className=" text-2xl font-bold">{title}</div>
-      <div className="font-light text-neutral-500 mt-20 ">{subTitle}</div>
+      <div className="font-light text-neutral-500 mt-20">{subTitle}</div>
     </div>
   );
 };
